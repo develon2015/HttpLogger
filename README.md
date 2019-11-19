82ff4db9dde1e01b1fc59fa0e9cc56e324e4d25e
kasumi <develon@qq.com>
2019-11-19 20:09:40 +0800

    v1.0

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ HttpLogger 带日志的HTTP服务
 
 * 友好的日志输出(lib.log.Logger)
 
+
+运行
+---
+执行命令`java -cp http.jar MainKt 0.0.0.0 80 ./www`即可
+
--- src/Main.kt
+++ src/Main.kt
@@ -4,6 +4,10 @@ import lib.http.HttpServer
 val logger = Logger("全局日志")
 
 fun main(args: Array<String>) {
-	val server = HttpServer("", 8080, "./www")
+	if (args.size < 3) {
+		println("请提供Host Port baseDir, 如: 0.0.0.0 80 ./www")
+		return
+	}
+	val server = HttpServer(args[0], args[1].toInt(), args[2])// HttpServer("", 8080, "./www")
 	server.service()
 }
